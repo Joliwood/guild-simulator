7a091009ecc1b7e636f0ddd77b96449191d04f6e
Guillaume Jolibois <jolibois.contact@gmail.com>
2024-06-29 10:43:35 +0200

    WIP

--- src/systems/inputs/mouse_click_system.rs
+++ src/systems/inputs/mouse_click_system.rs
@@ -6,34 +6,15 @@ use bevy::{
 };
 
 // This system prints messages when you press or release the left mouse button:
-pub fn mouse_click_system(
-    mouse_button_input: Res<ButtonInput<MouseButton>>,
-    // q_windows: Query<&Window, With<PrimaryWindow>>
-) {
-    // Left Mouse Button
+pub fn mouse_click_system(mouse_button_input: Res<ButtonInput<MouseButton>>) {
     if mouse_button_input.pressed(MouseButton::Left) {
-        // info!("Left mouse button currently pressed.");
-        // info!("{:?}", q_windows);
-        // if let Some(position) = q_windows.single().cursor_position() {
-        //     println!("Cursor is inside the primary window, at {:?}", position);
-        // } else {
-        //     println!("Cursor is not in the game window.");
-        // }
-    }
-    // if mouse_button_input.just_pressed(MouseButton::Left) {
-    //     info!("Left mouse button just pressed.");
-    // }
-    if mouse_button_input.just_released(MouseButton::Left) {
-        info!("Left mouse button just released.");
+        // window.set_cursor_icon(CursorIcon::Hand);
     }
 
-    // Right Mouse Button
-    // if mouse_button_input.pressed(MouseButton::Right) {
-    //     info!("Right mouse button currently pressed.");
-    // }
-    // if mouse_button_input.just_pressed(MouseButton::Right) {
-    //     info!("Right mouse button just pressed.");
-    // }
+    if mouse_button_input.just_released(MouseButton::Left) {
+        // info!("Left mouse button just released.");
+    }
+
     if mouse_button_input.just_released(MouseButton::Right) {
         info!("Right mouse button just released.");
     }
