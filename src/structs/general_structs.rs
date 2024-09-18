aeab3a58ae0719871659f911ae3fb00cba8fd503
Guillaume Jolibois <jolibois.contact@gmail.com>
2024-09-18 21:19:14 +0200

    Add guild level system
    - Add function to gain experience
    - Add function to level up + manage the left over xp

--- src/structs/general_structs.rs
+++ src/structs/general_structs.rs
@@ -10,17 +10,20 @@ pub struct UniqueId(pub String);
 
 #[derive(Component, Resource)]
 pub struct PlayerStats {
+    pub experience: u32,
     pub golds: i32,
+    pub guild_level: i8,
+    pub max_experience: u32,
     pub recruits: Vec<RecruitStats>,
     pub room: RoomEnum,
 }
 
 #[derive(Debug, Component, Clone, Eq, PartialEq, Hash)]
 pub struct RecruitStats {
-    pub id: Uuid,
     pub class: RecruitEnum,
     pub endurance: u16,
     pub experience: u32,
+    pub id: Uuid,
     pub intelligence: u16,
     pub level: u8,
     pub max_experience: u32,
@@ -33,17 +36,17 @@ pub struct SelectedRecruit(pub Option<RecruitStats>);
 #[derive(Resource, Debug, Component, Clone, Eq, PartialEq, Hash)]
 pub struct SelectedMission {
     pub mission: Option<Mission>,
-    pub recruit_id: Option<Uuid>,
     pub percent_of_victory: Option<u32>,
+    pub recruit_id: Option<Uuid>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Component, Resource)]
 pub struct Ennemy {
-    pub name: String,
     pub endurance: u16,
     pub experience: u32,
     pub intelligence: u16,
     pub level: u8,
+    pub name: String,
     pub strength: u16,
 }
 
@@ -51,10 +54,10 @@ pub struct Ennemy {
 pub struct Missions(pub Vec<Mission>);
 #[derive(Debug, Component, Clone, Eq, PartialEq, Hash)]
 pub struct Mission {
-    pub id: Uuid,
-    pub name: String,
-    pub level: u8,
     pub ennemy: Ennemy,
+    pub id: Uuid,
+    pub level: u8,
+    pub name: String,
 }
 
 // --- Implementations --- //
@@ -69,8 +72,8 @@ impl Default for SelectedMission {
     fn default() -> Self {
         Self {
             mission: None,
-            recruit_id: None,
             percent_of_victory: None,
+            recruit_id: None,
         }
     }
 }
@@ -79,14 +82,34 @@ impl PlayerStats {
     pub fn increment_golds(&mut self, amount: i32) {
         self.golds += amount;
     }
+
+    fn gain_xp(&mut self, xp: u32) {
+        self.experience += xp;
+
+        // Reset the experience with left experience after leveling up
+        // Then level up
+        if self.experience >= self.max_experience {
+            self.experience -= self.max_experience;
+            self.level_up();
+        }
+    }
+
+    pub fn level_up(&mut self) {
+        self.guild_level += 1;
+        // Set the max experience to the current experience * 2
+        self.max_experience *= 2;
+    }
 }
 
 impl Default for PlayerStats {
     fn default() -> Self {
         Self {
+            experience: 0,
             golds: 0,
-            room: RoomEnum::CommandRoom,
+            guild_level: 1,
+            max_experience: 100,
             recruits: vec![],
+            room: RoomEnum::CommandRoom,
         }
     }
 }
@@ -99,11 +122,11 @@ impl Default for Missions {
                 name: "Mission 1".to_string(),
                 level: 1,
                 ennemy: Ennemy {
-                    name: "Ennemy 1".to_string(),
                     endurance: 10,
                     experience: 0,
                     intelligence: 5,
                     level: 1,
+                    name: "Ennemy 1".to_string(),
                     strength: 10,
                 },
             },
@@ -112,11 +135,11 @@ impl Default for Missions {
                 name: "Mission 2".to_string(),
                 level: 2,
                 ennemy: Ennemy {
-                    name: "Ennemy 2".to_string(),
                     endurance: 15,
                     experience: 0,
                     intelligence: 7,
                     level: 2,
+                    name: "Ennemy 2".to_string(),
                     strength: 15,
                 },
             },
@@ -125,11 +148,11 @@ impl Default for Missions {
                 name: "Mission 3".to_string(),
                 level: 3,
                 ennemy: Ennemy {
-                    name: "Ennemy 3".to_string(),
                     endurance: 20,
                     experience: 0,
                     intelligence: 10,
                     level: 3,
+                    name: "Ennemy 3".to_string(),
                     strength: 20,
                 },
             },
