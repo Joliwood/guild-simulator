3a461c5af6f1208ebb01c30e0b2a27cfe88c6b2e
Guillaume Jolibois <jolibois.contact@gmail.com>
2024-09-20 10:23:41 +0200

    WIP add barrack nodes

--- src/ui/rooms/barrack/room.rs
+++ src/ui/rooms/barrack/room.rs
@@ -8,7 +8,7 @@ use crate::{
 };
 use bevy::prelude::*;
 
-pub fn room_barrack(
+pub fn spawn_room_barrack(
     asset_server: &Res<AssetServer>,
     commands: &mut Commands,
     player_stats: &Res<PlayerStats>,
@@ -35,57 +35,9 @@ pub fn room_barrack(
         .insert(Name::new("Room barrack"))
         .insert(ResetRoomTrigger)
         .insert(PlayerStatsRecruitsTrigger)
-        // Left container
+        // WIP - Spawn the left container
         .with_children(|parent| {
-            parent
-                .spawn(NodeBundle {
-                    style: Style {
-                        display: Display::Flex,
-                        flex_direction: FlexDirection::Column,
-                        justify_content: JustifyContent::Center,
-                        align_items: AlignItems::Center,
-                        width: Val::Percent(40.0),
-                        ..default()
-                    },
-                    ..default()
-                })
-                .insert(Name::new("Barrack > left container"))
-                .with_children(|left_container| {
-                    // Background image
-                    left_container.spawn(ImageBundle {
-                        image: image_handle.into(),
-                        style: Style {
-                            position_type: PositionType::Absolute,
-                            width: Val::Percent(100.0),
-                            height: Val::Percent(100.0),
-                            ..default()
-                        },
-                        ..default()
-                    });
-
-                    // Barrack room > left container > recruit buttons
-                    for recruit in player_stats.recruits.iter() {
-                        left_container
-                            .spawn(CustomButton::Primary.bundle(&asset_server, image_assets))
-                            .insert((
-                                UniqueId(format!("recruit_button_{}", recruit.id)),
-                                SelectedRecruitTrigger,
-                            )) // Use recruit.id here
-                            .with_children(|button| {
-                                button.spawn(TextBundle {
-                                    text: Text::from_section(
-                                        &recruit.class.to_string(),
-                                        TextStyle {
-                                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                                            font_size: 20.0,
-                                            color: Color::BLACK,
-                                        },
-                                    ),
-                                    ..default()
-                                });
-                            });
-                    }
-                });
+            spawn_left_container(parent, asset_server, player_stats, image_assets);
 
             // Right container: recruit info
             parent
--- src/ui/rooms/mod.rs
+++ src/ui/rooms/mod.rs
@@ -1,4 +1,7 @@
-pub mod room_barrack;
+pub mod barrack {
+    pub mod left_container;
+    pub mod room;
+}
 pub mod room_command_room;
 pub mod room_office;
 pub mod room_setup;
