cf7cc0143b8352f48e077f9fe29c01f0f749e8f8
Guillaume Jolibois <jolibois.contact@gmail.com>
2024-12-09 23:39:19 +0100

    WORKING STATE animated sprite 2D

--- src/ui/rooms/office/room_office.rs
+++ src/ui/rooms/office/room_office.rs
@@ -3,13 +3,43 @@ use super::{
     daily_event_documents::daily_event_documents,
     mission_report_documents::mission_report_documents,
 };
+use crate::my_assets;
 use crate::structs::{
     daily_events_folder::daily_events::DailyEvents, general_structs::MissionReportsModalVisible,
     missions::MissionReports, trigger_structs::ResetRoomTrigger,
 };
 use bevy::{prelude::*, text::cosmic_text::Align};
 use bevy_light_2d::prelude::*;
-use vleue_kinetoscope::AnimatedImageController;
+// use vleue_kinetoscope::AnimatedImageController;
+use bevy_light_2d::prelude::*;
+
+#[derive(Component)]
+pub struct AnimationIndices {
+    first: usize,
+    last: usize,
+}
+
+#[derive(Component, Deref, DerefMut)]
+pub struct AnimationTimer(Timer);
+
+pub fn animate_sprite(
+    time: Res<Time>,
+    mut query: Query<(&AnimationIndices, &mut AnimationTimer, &mut Sprite)>,
+) {
+    for (indices, mut timer, mut sprite) in &mut query {
+        timer.tick(time.delta());
+
+        if timer.just_finished() {
+            if let Some(atlas) = &mut sprite.texture_atlas {
+                atlas.index = if atlas.index == indices.last {
+                    indices.first
+                } else {
+                    atlas.index + 1
+                };
+            }
+        }
+    }
+}
 
 pub fn room_office(
     my_assets: &Res<AssetServer>,
@@ -17,12 +47,34 @@ pub fn room_office(
     mission_reports: &Res<MissionReports>,
     _mission_reports_modal_visibility: ResMut<MissionReportsModalVisible>,
     daily_events: &Res<DailyEvents>,
+    texture_atlas_layouts: &mut ResMut<Assets<TextureAtlasLayout>>,
 ) {
-    let animated_image = my_assets.load("images/rooms/office/giphy.webp");
+    // let animated_image = my_assets.load("images/rooms/office/giphy.webp");
+    let layout = TextureAtlasLayout::from_grid(
+        UVec2 {
+            x: 840 / 4,
+            y: 600 / 4,
+        },
+        4,
+        4,
+        None,
+        None,
+    );
+    let animation_indices = AnimationIndices { first: 1, last: 14 };
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
 
     commands
         .spawn((
-            AnimatedImageController::play(animated_image),
+            Sprite::from_atlas_image(
+                my_assets.load("images/rooms/office/palme-animation1.png"),
+                TextureAtlas {
+                    layout: texture_atlas_layout,
+                    index: animation_indices.first,
+                },
+            ),
+            animation_indices,
+            AnimationTimer(Timer::from_seconds(0.01, TimerMode::Repeating)),
+            // AnimatedImageController::play(animated_image),
             // Node {
             //     // display: Display::Flex,
             //     // align_self: AlignSelf::Center,
